9207841fe443e375d75833aac7a20b664b372496
Anne Marie <anne.saunders19@imperial.ac.uk>
2020-04-27 10:59:19 +0100

    Pushing Week 4 progress
    Former-commit-id: 4471655676b9c94bcbe04d770874a464624f1704

---
+++ Code/aggregation_batch.R
@@ -0,0 +1,6 @@
+#!/bin/env Rscript
+# Author: Anne Marie Saunders
+# Script: aggregation_batch.R
+# Desc: Use aggregation script on a set of datasets
+# Arguments: 
+# Date: 04/23/20
--- Code/floridats.R
+++ Code/floridats.R
@@ -2,20 +2,31 @@
 # Author: Anne Marie Saunders
 # Script: floridats.R
 # Desc: extract time series of climate data for a group of locations
-# Arguments: ../../../vectdyn_fulldata.csv
+# Arguments: ../../../fdcounts2.csv
 # Date: 04/16/19
 
-fddata = read.csv("~/Documents/Hefty_Data/mosquitoAbun/fdcounts.csv", header = T, stringsAsFactors = F)
+# Read in abundace dataset
+fddata = read.csv("~/Documents/Hefty_Data/mosquitoAbun/fdcounts2.csv", header = T, stringsAsFactors = F)
 
+# Create vector of locations
 counties = unique(fddata$Locations)
-i = 1
-for(i in 1:length(counties)){
-  print(counties[i])
-  vector_abun = fddata[fddata$Locations == counties[i],]
-  source("~/Documents/CMEEThesis/Code/climate_extraction_Matt.R")
-  filename = paste("~/Documents/CMEEThesis/Data/", counties[i], ".csv", sep = "")
-  write.csv(vector_abun, file = filename, row.names = F) # change to time series
-  rm(list=setdiff(ls(), c("i", "counties", "fddata")))
+
+# Extract climate for each location and save time series as csv
+for(p in 1:length(counties)){
+  vector_abun = fddata[fddata$Locations == counties[p],]
+  source("~/Documents/CMEEThesis/Code/climate_extraction_batch.R")
+  filename = paste("~/Documents/Hefty_Data/Extracted_Data/", locale,
+                    "_clim_TS.csv", sep = "")
+  write.csv(vector_abun_clim_time_series, file = filename, row.names = F)
+  rm(list=setdiff(ls(), c("p", "counties", "fddata")))
 }
 
-# correct extraction to save all necessary objects
+# Check that csvs look as expected
+# Add in whatever checks desired
+
+for(i in 1:length(counties)){
+  d = read.csv(paste0("~/Documents/Hefty_Data/Extracted_Data/", counties[i], "_clim_TS.csv"), nrows = 20000, header = T, stringsAsFactors = F)
+  cat(paste("\n", counties[i], "\n"))
+  str(d)
+  print(quantile(d$precip))
+}
